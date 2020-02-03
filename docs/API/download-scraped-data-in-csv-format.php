cadcde1ae6a89842c46e2d99ce9facdf3d9c5fc9
Martins Balodis <martins256@gmail.com>
2020-02-03 12:14:23 +0200

    fix < > characters in example files

--- docs/API/download-scraped-data-in-csv-format.php
+++ docs/API/download-scraped-data-in-csv-format.php
@@ -7,6 +7,11 @@ $outputFile = "/tmp/scrapingjob-data{$scrapingJobId}.csv";
 $client->downloadScrapingJobCSV($scrapingJobId, $outputFile);
 
 // read CSV file
+// NOTE! We recommend using json format since multiple CSV notations are
+// being used by different products. For example:
+// CSV Standard: https://tools.ietf.org/html/rfc4180
+// MS Excel cannot handle escape sequences from the CSV standard
+// PHP has incorrect default implementation. See https://wiki.php.net/rfc/kill-csv-escaping
 $records = Reader::createFromPath($outputFile)->fetchAssoc();
 
 foreach($records as $record) {
