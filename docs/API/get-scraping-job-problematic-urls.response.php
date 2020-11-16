18f2ce37433211062ca22ede77a44e0c772b5e86
Einars Ozols <einarsozolsozols@gmail.com>
2020-11-16 17:42:37 +0200

    WS-856 Problematic urls documentation

---
+++ docs/API/get-scraping-job-problematic-urls.response.php
@@ -0,0 +1,14 @@
+// response (iterator)
+[
+	[
+		'url' => 'https://webscraper.io/empty',
+		'type' => 'empty',
+	],
+	[
+		'url' => 'https://webscraper.io/failed',
+		'type' => 'failed',
+	],
+	[
+	...
+	],
+]
