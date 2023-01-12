08512b23a7864d7af92ce0a955858736282994a9
markussWs <markuss.bemers@webscraper.io>
2023-01-12 12:31:28 +0200

    WS-4876 api node client documentation fixes

--- docs/API/get-scraping-jobs.code.js
+++ docs/API/get-scraping-jobs.code.js
@@ -1,11 +1,11 @@
 let generator = client.getScrapingJobs({
-	sitemap_id: undefined,
+	sitemap_id: 123, // optional
 });
 const scrapingJobs = await generator.getAllRecords();
 
 // or iterate through all scraping jobs manually
 generator = client.getScrapingJobs({
-	sitemap_id: undefined,
+	sitemap_id: 123, // optional
 });
 for await (const record of await generator.fetchRecords()) {
 	console.log(JSON.stringify(record));
