0dfc12a4e84a605847001a4c8224fdc2dbf86212
Einars Ozols <einarsozolsozols@gmail.com>
2025-03-31 15:37:39 +0300

    WS-7756 Update documentation for proxy usage (#61)
    * WS-7756 Update documentation for proxy usage
    
    * WS-7756 Add residential-th

--- docs/API/get-sitemap-scheduler.response.php
+++ docs/API/get-sitemap-scheduler.response.php
@@ -1,6 +1,6 @@
 [
     'scheduler_enabled' => true,
-    'proxy' => 0, // 0: No proxy, 1: Use proxy, 123: Custom proxy id, 'residential-*': Use residential proxy, replace * with country code, for example, 'residential-us'
+    'proxy' => 'datacenter-us', //  'datacenter-*' or 'residential-*', replace '*' with country code, for example, 'residential-us'
     'cron_minute' => '*/10',
     'cron_hour' => '*',
     'cron_day' => '*',
